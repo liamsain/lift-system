51a4b0c99e0f11e91d0f3ba7dc9303a74e863682
Liam Kennedy <liamkennedy89@hotmail.com>
2019-02-09 20:06:35 +0000

    added people count input so user can choose how many people are in the sim

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 const canvas = document.getElementById("theCanvas");
 const ctx = canvas.getContext("2d");
-
+const peopleCountElement = document.getElementById("people-count-input");
 let entities = [];
 
 const building = new Building();
@@ -11,17 +11,18 @@ const peopleSpawnPoint = { x: building.x + building.height + 20, y: building.y +
 const people = [new Person(peopleSpawnPoint.x, peopleSpawnPoint.y, building)];
 entities = [...entities, ...people];
 const drawEntities = () => entities.forEach(x => x.draw(ctx));
-function draw() {
 
+function draw() {
+  if(entities.length - 2 < peopleCountElement.value){
+    console.log('adding entitiy');
+    entities.push(new Person(peopleSpawnPoint.x, peopleSpawnPoint.y, building));
+  } else if(entities.length - 2 > peopleCountElement.value && peopleCountElement.value > 2){
+    console.log('removing entity');
+     entities.pop();
+  }
   ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
   drawEntities();
   requestAnimationFrame(draw);
 }
-const addPerson = () => {
-  if(entities.length < 15){
-    entities.push(new Person(peopleSpawnPoint.x, peopleSpawnPoint.y, building))
-  }
-}
-const addEntityInterval = () => setInterval(addPerson, 10000);
-addEntityInterval();
 draw();
